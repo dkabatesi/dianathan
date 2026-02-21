772ba9c5edb1ee1cc2f314594cef7a5031457536
Divine Kabatesi <kabatesidivine@gmail.com>
2026-02-21 12:45:38 +0200

    fixed opening screen and visibility

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function copyNumber(number){
 
 navigator.clipboard.writeText(number);
 
-alert("Number copied");
+alert("Number copied successfully");
 
 }
 
@@ -29,7 +29,13 @@ var distance = weddingDate - now;
 
 var days = Math.floor(distance/(1000*60*60*24));
 
+var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+
+var minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+
+
 document.getElementById("countdown").innerHTML =
-days + " days remaining";
+days+" days "+hours+" hours "+minutes+" minutes";
+
 
 },1000);
