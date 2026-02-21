b63d44911eea4b25f36aebd548d1d0c966c0d3ec
Divine Kabatesi <kabatesidivine@gmail.com>
2026-02-21 12:19:21 +0200

    Full Dianathan wedding site

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-function copyNumber(number){
-navigator.clipboard.writeText(number);
-alert("Number copied");
-}
+// COUNTDOWN
 
-var weddingDate = new Date("December 12, 2026 00:00:00");
+var weddingDate = new Date("December 12, 2026 00:00:00").getTime();
 
-var x = setInterval(function(){
+var timer = setInterval(function(){
 
 var now = new Date().getTime();
 
 var distance = weddingDate - now;
 
-var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var days = Math.floor(distance / (1000*60*60*24));
+
+var hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+
+var minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
 
 document.getElementById("countdown").innerHTML =
-days + " days to go";
+days + " days " + hours + " hours " + minutes + " minutes";
 
 },1000);
+
+
+// COPY NUMBER
+
+function copyNumber(number){
+
+navigator.clipboard.writeText(number);
+
+alert("Number copied successfully!");
+
+}
+
+
+// SCROLL
+
+function scrollToSupport(){
+
+document.querySelector(".support").scrollIntoView({
+
+behavior: "smooth"
+
+});
+
+}
